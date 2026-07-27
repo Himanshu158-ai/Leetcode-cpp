026bb9585f4121bbb3a3917d278421b602f8998e
Himanshu Singh <hs5317610@gmail.com>
2026-07-27 19:58:39 +0530

    Time: 4 ms (37.51%), Space: 14.1 MB (76.97%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
-    void helper(vector<int>& candidates, int target, set<vector<vector<int>>>& s, int i, vector<int>values, vector<vector<int>>& ans){
-        if(i>=candidates.size() || target<0){
-            return;
-        }
-        if(target==0){
-            if(s.find({values})==s.end()){
-                s.insert({values});
-                ans.push_back(values);
-            }
+    void helper(vector<int>& candidates, int target, int i,
+                vector<int>& curr, vector<vector<int>>& ans) {
+
+        // Base Case
+        if (target == 0) {
+            ans.push_back(curr);
             return;
         }
 
-        values.push_back(candidates[i]);
-        helper(candidates,target-candidates[i],s,i+1,values,ans);
-        helper(candidates,target-candidates[i],s,i,values,ans);
-        values.pop_back();
-        helper(candidates,target,s,i+1,values,ans);
-        return;
+        if (i == candidates.size() || target < 0)
+            return;
+
+        // ---------------- PICK ----------------
+        curr.push_back(candidates[i]);
+
+        // i same rahega kyunki same element ko fir se use kar sakte hain
+        helper(candidates, target - candidates[i], i, curr, ans);
+
+        curr.pop_back();
+
+        // ---------------- NOT PICK ----------------
+        // Ab current element ko permanently skip kar diya
+        helper(candidates, target, i + 1, curr, ans);
     }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        set<vector<vector<int>>>s;
-        vector<vector<int>>ans;
-        vector<int>values;
-        helper(candidates,target,s,0,values,ans);
+        vector<vector<int>> ans;
+        vector<int> curr;
 
-
+        helper(candidates, target, 0, curr, ans);
 
         return ans;
     }
