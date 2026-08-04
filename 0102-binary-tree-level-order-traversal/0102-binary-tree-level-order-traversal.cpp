db31f08875302a17a82b64997d9973964d974cb8
Himanshu Singh <hs5317610@gmail.com>
2026-08-04 11:34:02 +0530

    Time: 0 ms (100%), Space: 17.3 MB (16.29%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -15,17 +15,25 @@ public:
     void printer(queue<TreeNode*> &q, vector<vector<int>>& ans){
         if(q.empty())return;
 
-        vector<TreeNode*>vec;
+        // vector<TreeNode*>vec;
+        int size = q.size();
         vector<int>store;
-        while(!q.empty()){
-            vec.push_back(q.front());
-            q.pop();
-        }
+        // while(!q.empty()){
+        //     vec.push_back(q.front());
+        //     q.pop();
+        // }
 
-        for(int i = 0; i<vec.size(); i++){
-            store.push_back(vec[i]->val);
-            if(vec[i]->left)q.push(vec[i]->left);
-            if(vec[i]->right)q.push(vec[i]->right);
+        // for(int i = 0; i<vec.size(); i++){
+        //     store.push_back(vec[i]->val);
+        //     if(vec[i]->left)q.push(vec[i]->left);
+        //     if(vec[i]->right)q.push(vec[i]->right);
+        // }
+        while(size!=0){
+            store.push_back(q.front()->val);
+            if(q.front()->left)q.push(q.front()->left);
+            if(q.front()->right)q.push(q.front()->right);
+            q.pop();
+            size--;
         }
         ans.push_back(store);
         printer(q,ans);
