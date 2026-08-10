e7729bd7fd2ca3fab16c46ccf3fcf1db9cd6615e
Himanshu Singh <hs5317610@gmail.com>
2026-08-10 13:19:11 +0530

    Time: 0 ms (100%), Space: 37.5 MB (40.02%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -9,26 +9,79 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
+// class Solution {
+// public:
+//     void fun(TreeNode* root, vector<int>& ans){
+//         if(root==NULL) return;
+
+//         fun(root->left,ans);
+//         ans.push_back(root->val);
+//         fun(root->right,ans);
+//         return;
+//     }
+
+//     bool findTarget(TreeNode* root, int k) {
+//         vector<int> ans;
+//         fun(root,ans);
+//         int i = 0, j = (int)ans.size()-1;
+//         while(i<j){
+//             if(ans[i]+ans[j]==k) return true;
+//             else if(ans[i]+ans[j]>k) j--;
+//             else i++;
+//         }
+//         return false;
+//     }
+// };
+
+
 class Solution {
 public:
-    void fun(TreeNode* root, vector<int>& ans){
+
+    void smallest(TreeNode* root,stack<TreeNode*>& small){
         if(root==NULL) return;
 
-        fun(root->left,ans);
-        ans.push_back(root->val);
-        fun(root->right,ans);
+        small.push(root);
+        smallest(root->left,small);
         return;
     }
+    void greatest(TreeNode* root,stack<TreeNode*>& great){
+        if(root==NULL) return;
 
+        great.push(root);
+        greatest(root->right,great);
+        return;
+    }
+    
     bool findTarget(TreeNode* root, int k) {
-        vector<int> ans;
-        fun(root,ans);
-        int i = 0, j = ans.size()-1;
-        while(i<j){
-            if(ans[i]+ans[j]==k) return true;
-            else if(ans[i]+ans[j]>k) j--;
-            else i++;
+        if(root==NULL) return false;
+        stack<TreeNode*>small;
+        stack<TreeNode*>great;
+
+        smallest(root,small);
+        greatest(root,great);
+
+        int greatVal = great.top()->val;
+        int smallVal = small.top()->val;
+        
+        while(greatVal>smallVal){
+            int sum = greatVal+smallVal;
+            if(sum<k){
+                TreeNode* temp = small.top();
+                small.pop();
+                smallest(temp->right,small);
+                if(small.empty()) return false;
+                smallVal = small.top()->val;
+            }else if(sum>k){
+                TreeNode* temp = great.top();
+                great.pop();
+                greatest(temp->left,great);
+                if(great.empty()) return false;
+                greatVal = great.top()->val;
+            }else return true;
         }
+
         return false;
     }
 };
