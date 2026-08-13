dccb201afa8aef8cda205aca129f60917332a90d
Himanshu Singh <hs5317610@gmail.com>
2026-08-13 19:42:37 +0530

    Time: 0 ms (100%), Space: 13.7 MB (84.53%) - LeetHub

---
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    bool fun(queue<TreeNode*> &que){
+        
+        if(que.empty()) return true;
+        
+        int size = que.size();
+
+        while(size){
+            TreeNode * node = que.front();
+            que.pop();
+
+
+            if(!node){
+                while(!que.empty()){
+                    if(que.front()!=NULL) return false;
+                    que.pop();
+                }
+                return true;
+            }
+            // if(!node->left && !node->right){
+            //     size--;
+            //     continue;
+            // };
+
+            if(node->left) que.push(node->left);
+            else que.push(NULL);
+
+            if(node->right) que.push(node->right);
+            else que.push(NULL);
+
+            size--;
+        }
+        if(!fun(que)) return false;
+
+        return true;
+
+
+    }
+    
+    bool isCompleteTree(TreeNode* root) {
+        queue<TreeNode*> que;
+        que.push(root);
+        return fun(que);
+    }
+};
