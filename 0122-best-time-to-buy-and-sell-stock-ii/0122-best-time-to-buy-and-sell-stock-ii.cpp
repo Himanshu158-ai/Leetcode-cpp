4df4f5d5d26911393fe350984e6724ad6b1396d1
Himanshu Singh <hs5317610@gmail.com>
2026-08-26 17:58:23 +0530

    Time: 0 ms (100%), Space: 20.1 MB (65.78%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,34 +1,49 @@
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int n = prices.size();
+//         vector<int> temp(n);
+//         stack<int> st;
+//         int i = n-1;
+
+//         while(i>=0){
+//             while(!st.empty()){
+//                 if(prices[i]>st.top()){
+//                     st.pop();
+//                 }else{
+//                     temp[i] = st.top();
+//                     st.pop();
+//                     st.push(prices[i]);
+//                     break;
+//                 }
+//             }
+//             if(st.empty()){
+//                 temp[i] = prices[i];
+//                 st.push(prices[i]);
+//             }
+//             i--;
+//         }
+
+//         int ans = 0;
+//         for(int j = 0; j<prices.size(); j++){
+//             int val = temp[j]-prices[j];
+//             ans+=val;
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        vector<int> temp(n);
-        stack<int> st;
-        int i = n-1;
-
-        while(i>=0){
-            while(!st.empty()){
-                if(prices[i]>st.top()){
-                    st.pop();
-                }else{
-                    temp[i] = st.top();
-                    st.pop();
-                    st.push(prices[i]);
-                    break;
-                }
-            }
-            if(st.empty()){
-                temp[i] = prices[i];
-                st.push(prices[i]);
-            }
-            i--;
-        }
-
         int ans = 0;
-        for(int j = 0; j<prices.size(); j++){
-            int val = temp[j]-prices[j];
-            ans+=val;
+
+        for(int i = 1; i < prices.size(); i++) {
+            if(prices[i] > prices[i - 1]) {
+                ans += prices[i] - prices[i - 1];
+            }
         }
+
         return ans;
     }
 };
