49ebb010b4f7a0782128918f9fa9d0896d6a3b70
Himanshu Singh <hs5317610@gmail.com>
2026-08-28 19:24:13 +0530

    Time: 0 ms (100%), Space: 16.4 MB (96.71%) - LeetHub

---
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+
+        ListNode* temp = head;
+
+        // Check whether k nodes are available
+        for(int i = 0; i < k; i++) {
+            if(temp == nullptr)
+                return head;
+
+            temp = temp->next;
+        }
+
+        // Reverse first k nodes
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+
+        for(int i = 0; i < k; i++) {
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        // head is now the last node of reversed group
+        head->next = reverseKGroup(curr, k);
+
+        return prev;
+    }
+};
