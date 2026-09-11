03868f23bb221cabd34b1bbe0387558f4909fd62
Himanshu Singh <hs5317610@gmail.com>
2026-09-11 20:37:32 +0530

    Time: 2 ms (15.22%), Space: 8.8 MB (40.36%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-        unordered_map<char,int>mapPattern;
-        unordered_map<string,int>mapS;
+        vector<string> words;
         int i = 0;
-        int j = 0;
-        int k = 0;
-        while(i<pattern.size()){
-            string str = "";
-            while(j < s.size() && s[j] != ' '){
-                str+=s[j];
-                j++;
+        while(i < s.size()) {
+            string t = "";
+            while(s[i] != ' ' && i < s.size()) {
+                t += s[i];
+                i++;
             }
-            if(str == "") return false;
-            if(mapPattern.find(pattern[i])==mapPattern.end()){
-                mapPattern[pattern[i]] = i;
-            }
-            if(mapS.find(str)==mapS.end()){
-                mapS[str] = k;
-            }
-
-            if(mapPattern[pattern[i]]!=mapS[str]) return false;
-            i++,j++,k++;
+            words.push_back(t);
+            i++;
         }
-        if(j < s.size()) return false;
+        if(pattern.size() != words.size()) return false;
+        unordered_map<char, string> pw;
+        unordered_map<string, char> wp;
+        for(int i = 0; i < pattern.size(); i++) {
+            if(pw.count(pattern[i]) == 0) pw[pattern[i]] = words[i];
+            if(wp.count(words[i]) == 0) wp[words[i]] = pattern[i];
+            if(pw[pattern[i]] != words[i] || wp[words[i]] != pattern[i]) return false;
+        }
         return true;
     }
 };
