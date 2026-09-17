ac6ac47f5e7fe7ddd363f040e8ce71141f9b87b6
Himanshu Singh <hs5317610@gmail.com>
2026-09-17 20:36:23 +0530

    Time: 0 ms (100%), Space: 18.4 MB (82.54%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,7 +10,6 @@ public:
 
         priority_queue<ListNode*, vector<ListNode*>, compare> pq;
 
-        // Har linked list ka first node heap me daalo
         for (ListNode* head : lists) {
             if (head != nullptr) {
                 pq.push(head);
@@ -22,15 +21,13 @@ public:
 
         while (!pq.empty()) {
 
-            // Sabse chhota node
             ListNode* node = pq.top();
             pq.pop();
 
-            // Result list me add
             tail->next = node;
             tail = tail->next;
 
-            // Us list ka next node heap me daalo
+
             if (node->next != nullptr) {
                 pq.push(node->next);
             }
